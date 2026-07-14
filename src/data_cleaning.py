e07ccb0d7fe87dbfee6541cced50e319c7b66f11
KALAMPEE <gunmidbro@gmail.com>
2026-07-14 15:28:29 +0700

    Add rename and convert data type to data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -2,9 +2,17 @@ import pandas as pd
 
 class TextCleaner:
     
-    def processing(self, df:pd.DataFrame ) -> pd.DataFrame:
+    def rename_and_map(self, df:pd.DataFrame ) -> pd.DataFrame:
         data = df.copy()
-        rename_df = data.rename(columns = {0:'reviews',1:'title',2:'text'})
-        rename_df.replace({'reviews':{1:'negative',2:'positive'}},inplace=True)
+        rename_df = data.rename(columns = {0:'sentiment',1:'title',2:'text'})
+        rename_df.replace({'sentiment':{1:'negative',2:'positive'}},inplace=True)
         return rename_df
+
+    def convert_dtypes(self,df:pd.DataFrame):
+        df['sentiment'] = df['sentiment'].astype('category')
+        return df
     
+    def processing(self,df):
+        data = self.rename_and_map(df)
+        data = self.convert_dtypes(data)
+        return data
